becd9727c16f944c09795d04c4da887e81b4575d
july183 <julysmj183@gmail.com>
2022-02-28 11:55:09 +0900

    hostset

---
+++ routes/router.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req, res){
+    res.send('./user라고 하면 내가 나옴');
+    
+});
+router.get('/',function(req, res){
+    res.send('./user/company라고 하면 내가 나옴');
+    
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var projectlink = require('./routes/router');
 var app = express();
 
 
-app.listen("5000")
+app.get('/',function(req, res){
+    res.send('글자노출하게 됨');
+
+});
+app.use('/user',projectlink);
+
+app.listen(5000, function(){
+    console.log('서버구동완료')
+});
