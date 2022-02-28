08d57af4dd0595947acbe784106ffe2d7d450686
july183 <julysmj183@gmail.com>
2022-02-28 17:46:13 +0900

    proxy_react

--- routes/router.js
+++ routes/router.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req, res){
-    res.send('./user라고 하면 내가 나옴');
+    res.send('/user라고 하면 내가 나옴');
     
 });
-router.get('/',function(req, res){
-    res.send('./user/company라고 하면 내가 나옴');
+router.get('/company',function(req, res){
+    res.send('/user/company라고 하면 내가 나옴');
     
 });
 
--- server.js
+++ server.js
@@ -9,6 +9,27 @@ app.get('/',function(req, res){
 });
 app.use('/user',projectlink);
 
-app.listen(5000, function(){
+// app.listen(5000, function(){
+//     console.log('서버구동완료')
+// });
+
+app.get('/me',function(req, res){
+    res.send('나의 소개');
+
+});
+app.get('/protfolio',function(req, res){
+    res.send('작품들');
+
+});
+app.get('/preinterview',function(req, res){
+    res.send('사전면접');
+
+});
+app.get('/contact',function(req, res){
+    res.send('면접제안');
+
+});
+
+app.listen(8080, function(){
     console.log('서버구동완료')
 });
